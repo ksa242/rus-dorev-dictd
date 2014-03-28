0cc02442762f844ce9f8bcb642ebd720db1c8cea
Sergey Kolchin <me@ksa242.name>
2014-03-29 00:42:12 +0400

    Allow building the dictionary with truly arbitrary names.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#! /bin/sh
 
 NAME=rus-dorev
 
-dictfmt -t --utf8 --without-ver ${NAME} < ${NAME}.txt &&
-dictzip ${NAME}.dict &&
-tar -czf dictd-${NAME}.tar.gz ${NAME}.dict.dz ${NAME}.index
+dictfmt -t --utf8 --without-ver "${NAME}" < "${NAME}.txt" &&
+dictzip "${NAME}.dict" &&
+tar -czf "dictd-${NAME}.tar.gz" "${NAME}.dict.dz" "${NAME}.index"
